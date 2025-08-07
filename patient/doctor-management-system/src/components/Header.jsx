eb7523e1da34a97e9533d6b68e9df7fb02836cc8
sanjiv-65 <mrsanjiv105@gmail.com>
2025-08-08 00:18:21 +0530

    home

--- patient/doctor-management-system/src/components/Header.jsx
+++ patient/doctor-management-system/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
             <p> Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> schedule your appointment hassle-free </p>
           </div>
           <a href="#speciality" className=' flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
-            Book appointment <img classNmae='w-3' src={assets.arrow_icon} alt="" />
+            Book appointment <img className='w-3' src={assets.arrow_icon} alt="" />
           </a>
     </div>
     
