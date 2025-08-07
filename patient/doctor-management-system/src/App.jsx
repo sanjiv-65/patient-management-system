7d44b856f98568c53055e5eadc496299ddd34286
sanjiv-65 <mrsanjiv105@gmail.com>
2025-08-07 20:00:57 +0530

    home

--- patient/doctor-management-system/src/App.jsx
+++ patient/doctor-management-system/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -25,11 +26,9 @@ const App = () => {
          <Route path='/my-appointments' element={<MyAppointments />} />
          <Route path='/appointments/:docId' element={<Appointment />} />
          
-
-        
-
-
       </Routes>
+    <Footer />
+      
     </div>
   )
 }
