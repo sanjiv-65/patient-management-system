716b350f7106a554ccce911cc874f96c794ce9f0
sanjiv-65 <mrsanjiv105@gmail.com>
2025-10-09 00:27:21 +0530

    img and hs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>
       <div className='my-10  flex flex-col md:flex-row gap-12'>
-        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <img className='w-full md:max-w-[360px] ' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>A hospital is a vital part of any hospital is a vital part of any community, serv to maternity care, diagnostics, and community, serv to maternity care, diagnostics, and outpatient ed equipment and staffed by trained professionals, ensuring that patients receive timely and effective care.</p>
           <p>Beyond treating illness, hospitals play an important role in health education and dis, cont hospital is a vital part of any community, serv to maternity care, diagnostics, andributing to the overall advancement of medicine.</p>
